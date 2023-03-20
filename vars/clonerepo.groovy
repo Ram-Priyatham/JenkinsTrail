a321ffae83b290a04cb58ceabf45b063cddb7417
Ram Priyatham <75263348+Ram-Priyatham@users.noreply.github.com>
2023-03-20 23:26:06 +0530

    Create clonerepo.groovy

---
+++ vars/clonerepo.groovy
@@ -0,0 +1,7 @@
+def call(version){
+  def tag = params.AppVersion
+  echo "${params.AppVersion}"
+  // sh  "/usr/bin/git clone --branch ${params.AppVersion} https://github.com/Ram-Priyatham/JenkinsDockerIntegration/"
+  git(branch: 'master', url: 'https://github.com/Ram-Priyatham/JenkinsDockerIntegration')
+  sh "git checkout tags/${params.AppVersion} "
+}
